3e531db1a0a55e0469c3d01bb450adfe8dbcea9d
That910fella <mariobarron0@gmail.com>
2025-10-13 15:18:11 -0400

    Create main.py

---
+++ src/barronai/core/main.py
@@ -0,0 +1,16 @@
+from loguru import logger
+import time
+import os
+
+def heartbeat():
+    logger.info("Barron.AI service booting...")
+    logger.info(f"ENV={os.getenv('ENV','dev')}")
+    while True:
+        logger.info("heartbeat: Barron.AI is alive")
+        time.sleep(10)
+
+if __name__ == "__main__":
+    try:
+        heartbeat()
+    except KeyboardInterrupt:
+        logger.info("Shutting down cleanly.")
