d7bfce6cfa60f5708516d8a99ea9caf730b9c58f
That910fella <mariobarron0@gmail.com>
2025-10-14 16:51:07 +0000

    fix: Procfile, core package, heartbeat

--- src/barronai/core/main.py
+++ src/barronai/core/main.py
@@ -1,10 +1,9 @@
 from loguru import logger
-import time
-import os
+import time, os
 
 def heartbeat():
     logger.info("Barron.AI service booting...")
-    logger.info(f"ENV={os.getenv('ENV','dev')}")
+    logger.info(f"ENV={os.getenv('ENV', 'dev')}")
     while True:
         logger.info("heartbeat: Barron.AI is alive")
         time.sleep(10)
